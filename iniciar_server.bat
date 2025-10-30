4ea6dce6e9d36c3bce1a85ab80cf83c3e67edbec
ENEZE <jg048.rocha@gmail.com>
2025-10-29 21:25:37 -0300

    Initial commit do servidor

---
+++ iniciar_server.bat
@@ -0,0 +1,25 @@
+@echo off
+title Servidor Minecraft
+color 0a
+echo ==============================================
+echo        SECOND SERVER INICIANDO...
+echo ==============================================
+echo.
+echo Abrindo  Second server...
+echo.
+echo Abrindo Second Server com 10 Gigas Iniciais e de Começo 6gb.
+echo.
+echo Abrindo em 3 Segundos...
+echo.
+echo.
+echo.
+echo.
+echo. 
+
+java -Xmx10G -Xms6G -jar server.jar nogui
+
+echo.
+echo ==============================================
+echo SERVIDOR SECOND FINALIZADO OU PARADO MANUALMENTE
+echo ==============================================
+pause
